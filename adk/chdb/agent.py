8a046b9bd08121d3c394a21de6fdfa8118b69514
Matt Franz <mdfranz@gmail.com>
2025-11-08 22:21:37 -0500

    adk run works

---
+++ adk/chdb/agent.py
@@ -0,0 +1,19 @@
+from google.adk.agents import Agent
+from google.adk.models.lite_llm import LiteLlm
+from google.adk.tools.mcp_tool.mcp_toolset import (
+    MCPToolset,
+    StreamableHTTPConnectionParams,
+)
+
+tools = MCPToolset(
+    connection_params=StreamableHTTPConnectionParams(url="http://localhost:8000/mcp")
+)
+
+root_agent = Agent(
+    name="dumb",
+    model=LiteLlm(model='openai/gpt-5-mini'),
+    tools=[tools],
+    description="Clickhouse Instructions",
+    instruction="""Create the necessary clickhouse query using the Tools to be able to query ClickHouse dat """
+)
+

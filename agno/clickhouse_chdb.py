78ba101bdb9436e7b3e95eddf933ee1a79eeb584
Matthew Franz <mdfranz@gmail.com>
2025-11-08 19:19:59 -0500

    Forgot agno

---
+++ agno/clickhouse_chdb.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import asyncio, sys, time
+
+from agno.agent import Agent
+from agno.models.anthropic import Claude
+from agno.models.ollama import Ollama
+from agno.models.google import Gemini
+from agno.models.openai import OpenAIChat
+from agno.tools.mcp import MCPTools
+
+#
+# Assumptions
+# uv run --with mcp-clickhouse mcp-clickhouse
+# export CLICKHOUSE_ENABLED=false
+# export CHDB_DATA_PATH=data
+# export CHDB_ENABLED=true
+# export CLICKHOUSE_MCP_SERVER_TRANSPORT=http
+#
+
+
+async def main(data_question, model):
+    async with MCPTools(
+        transport="streamable-http",
+        url="http://127.0.0.1:8000/mcp",
+        timeout_seconds=60,
+    ) as mcp_tools:
+        if model == "claude":
+            my_model = Claude(id="claude-sonnet-4-20250514")
+        elif model == "gemini":
+            my_model = Gemini(id="gemini-2.5-pro")
+        elif model.find("gpt") > -1:
+            my_model = OpenAIChat(id=model)
+        else:
+            my_model = Ollama(id=model)
+
+        agent = Agent(
+            model=my_model,
+            debug_mode=True,
+            markdown=True,
+            tools=[mcp_tools],
+        )
+        await agent.aprint_response(data_question, stream=True)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: foo.py <model>")
+        print("Example: foo.py claude")
+        print("Supported models: claude, gemini, gpt-*, ollama_model_name")
+        sys.exit(1)
+    model = sys.argv[1]
+    print(f"Selected {model}")
+    time.sleep(3)
+    prompt = """Using file('/tmp/mcp/data/**/*.log.gz', 'JSONEachRow') as a data source you will find JSON compressed files.
+    Find and count the different type of events based on event_type. Then find the number number of SSH logins based on the auth event_type. Lastly find the number of unique users that logged in via SSH
+        """
+    asyncio.run(main(prompt, model))
